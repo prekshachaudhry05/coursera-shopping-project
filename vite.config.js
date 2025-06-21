269ba3b4cd799352e203ef11af87fcffe4be0d08
Preksha Chaudhary <125497722+prekshachaudhry05@users.noreply.github.com>
2025-06-21 20:24:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/coursera-shopping-project/'
-})
+  base: '/coursera-shopping-project/', 
+});
