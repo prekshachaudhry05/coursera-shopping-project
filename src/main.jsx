34426a0a328f74e6431025c7730142378eaa838f
Preksha Chaudhary <125497722+prekshachaudhry05@users.noreply.github.com>
2025-06-22 17:49:49 +0530

    fixing index.css file path

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store'; // ✅ make sure this is correct
 import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/coursera-shopping-project">
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter basename="/coursera-shopping-project">
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
